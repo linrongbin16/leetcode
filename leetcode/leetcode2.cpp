64c4b4bbd898918a70399c9d3494a1972823450c
damu <damu@pinduoduo.com>
2019-01-16 19:08:04 +0800

    leetcode-2

--- leetcode/leetcode2.cpp
+++ leetcode/leetcode2.cpp
@@ -14,18 +14,30 @@ void dumpList(ListNode *l) {
   cout << endl << "[ ";
   while (l) {
     cout << l->val << " ";
+    l = l->next;
   }
   cout << "]";
 }
 
 int main(void) {
+  int x;
   Solution s;
+  ListNode *l1, *l2;
 
   int s1[] = {2, 4, 3};
   int s2[] = {5, 6, 4};
-  ListNode *l1 = createList(s1, 3), *l2 = createList(s2, 3);
+  l1 = createList(s1, 3);
+  l2 = createList(s2, 3);
   dumpList(s.addTwoNumbers(l1, l2));
 
+  int s3[] = {5};
+  int s4[] = {5};
+  int x;
+  l1 = createList(s3, 1);
+  l2 = createList(s4, 1);
+  dumpList(s.addTwoNumbers(l1, l2));
+
+  cin >> x;
   return 0;
 }
 
--- leetcode/leetcode2.h
+++ leetcode/leetcode2.h
@@ -24,7 +24,7 @@ public:
   }
 
   ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-    ListNode *p1 = reverseList(l1), *p2 = reverseList(l2);
+    ListNode *p1 = l1, *p2 = l2;
     ListNode *r = NULL;
     int add = 0, x;
 
@@ -32,14 +32,20 @@ public:
       x = (add + p1->val + p2->val) % 10;
       add = (add + p1->val + p2->val) / 10;
       r = insertHead(r, x);
+      p1 = p1->next;
+      p2 = p2->next;
     }
     ListNode *p3 = p1 ? p1 : p2;
     while (p3) {
       x = (add + p3->val) % 10;
       add = (add + p3->val) / 10;
       r = insertHead(r, x);
+      p3 = p3->next;
     }
-    return r;
+    if (add != 0) {
+      r = insertHead(r, add);
+    }
+    return reverseList(r);
   }
 };
 
