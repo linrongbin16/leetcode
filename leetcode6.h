7e820715f94aaf2db737a6c6138ab9e2695b5e1e
linrongbin <linrongbin16@gmail.com>
2019-01-22 09:50:39 +0800

    save code

--- leetcode6.h
+++ leetcode6.h
@@ -4,23 +4,25 @@ class Solution {
 public:
   string convert(string s, int numRows) {
     string result = s;
+
+    int p = 1, next;
+    result[0] = s[0];
     for (int i = 0; i < numRows; i++) {
-      int j = i;
-      int k = 0;
-      result[j] = s[i + k];
-      k++;
-      while (j < s.length()) {
-        j += (2 * numRows - 2 - 2 * i);
-        result[j] = s[i + k];
-        k++;
-        j += 2 * i;
-        if (j >= s.length()) {
+      int len = 2 * (numRows - i) - 1;
+      next = (i + 1) * len - 1;
+      if (next >= s.length()) {
+        break;
+      }
+      result[p++] = s[next];
+      if (i > 0) {
+        next += 2 * i;
+        if (next >= s.length()) {
           break;
         }
-        result[j] = s[i + k];
-        k++;
+        result[p++] = s[next];
       }
     }
+
     return result;
   }
 };
