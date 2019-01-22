6ff2200a5a0deb314340f8d325370e496fcffc42
linrongbin <linrongbin16@gmail.com>
2019-01-23 00:32:17 +0800

    lc-7 pass test

--- ReverseInteger-7.cpp
+++ ReverseInteger-7.cpp
@@ -1,4 +1,12 @@
 #include "ReverseInteger-7.h"
 
-int main(void) { return 0; }
+int main(void) {
+  Solution s;
+  // cout << s.reverse(123) << endl;
+  // cout << s.reverse(-123) << endl;
+  // cout << s.reverse(120) << endl;
+  cout << s.reverse(1534236469) << endl;
+  pause();
+  return 0;
+}
 
--- ReverseInteger-7.h
+++ ReverseInteger-7.h
@@ -2,6 +2,16 @@
 
 class Solution {
 public:
-  int reverse(int x) { return 0; }
+  int reverse(int x) {
+    int64_t result = 0;
+    while (x != 0) {
+      result = result * 10L + (int64_t)(x % 10);
+      x /= 10;
+    }
+    if (result > (int64_t)INT_MAX || result < (int64_t)INT_MIN) {
+      return 0;
+    }
+    return result;
+  }
 };
 
