749c8cdad31ecc111f2a001ad81ef81ee418e02e
damu <damu@pinduoduo.com>
2019-01-23 16:28:07 +0800

    save code

--- StringToIntegerAtoi-8.h
+++ StringToIntegerAtoi-8.h
@@ -2,6 +2,37 @@
 
 class Solution {
 public:
-  int myAtoi(string str) {}
+  bool isWhiteSpace(char c) { return c == ' '; }
+  bool isPlusMinus(char c) { return c == '+' || c == '-'; }
+  bool isPlus(char c) { return c == '+'; }
+  bool isMinus(char c) { return c == '-'; }
+  bool isDigit(char c) { return (int)c >= (int)'0' && (int)c <= (int)'9'; }
+
+  int myAtoi(string str) {
+    int result = 0;
+    bool negative = false;
+    bool startParsing = false;
+    for (int i = 0; i < str.length(); i++) {
+      if (isWhiteSpace(str[i])) {
+        if (!startParsing) {
+          continue;
+        } else {
+          break;
+        }
+      }
+
+      startParsing = true;
+      if (isPlusMinus(str[i])) {
+        if (i - 1 >= 0 && isWhiteSpace(str[i - 1])) {
+          if (isMinus(str[i])) {
+            negative = true;
+          }
+        }
+        if (i - 1 >= 0 && !isWhiteSpace(str[i - 1])) {
+        }
+      }
+    }
+    return result;
+  }
 };
 
