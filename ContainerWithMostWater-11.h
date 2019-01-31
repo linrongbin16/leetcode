6d6e4d2879eaa7fe8d120e55e772b738f92a9a10
damu <damu@pinduoduo.com>
2019-01-31 11:15:06 +0800

    save code

--- ContainerWithMostWater-11.h
+++ ContainerWithMostWater-11.h
@@ -4,14 +4,11 @@ class Solution {
 public:
   int maxArea(vector<int> &height) {
     int result = 0;
-    int *f = new int[height.size() + 1];
-    memset(f, 0, (height.size() + 1) * sizeof(int));
     for (int i = 0; i < height.size(); i++) {
       for (int j = i + 1; j < height.size(); j++) {
         result = max(result, min(height[i], height[j]) * (j - i));
       }
     }
-    delete[] f;
     return result;
   }
 };
