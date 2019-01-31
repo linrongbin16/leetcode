57afaed22e94c6a2e6493be5791fadd6e93de10d
damu <damu@pinduoduo.com>
2019-01-31 17:32:01 +0800

    update folder

---
+++ linked-list/LinkedListCycle-141.h
@@ -0,0 +1,13 @@
+#include "std.h"
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  bool hasCycle(ListNode *head) {}
+};
+
