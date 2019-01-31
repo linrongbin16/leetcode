1358ead22227eb398c775cf773a828c99ca80dd6
damu <damu@pinduoduo.com>
2019-01-31 16:27:46 +0800

    pass lc-143 test

--- RecorderList-143.h
+++ RecorderList-143.h
@@ -8,6 +8,24 @@ struct ListNode {
 
 class Solution {
 public:
-  void reorderList(ListNode *head) {}
+  void reorderList(ListNode *head) {
+    if (!head || !head->next) {
+      return;
+    }
+    vector<ListNode *> ptr;
+    ListNode *p = head;
+    while (p) {
+      ptr.push_back(p);
+      p = p->next;
+    }
+    p = head;
+    for (int i = 0; i < ptr.size() / 2; i++) {
+      ListNode *peer = ptr[ptr.size() - i - 1];
+      peer->next = p->next;
+      p->next = peer;
+      p = peer->next;
+    }
+    p->next = NULL;
+  }
 };
 
