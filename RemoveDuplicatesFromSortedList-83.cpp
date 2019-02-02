5f10a3af377147201e5bf9fef64d31004581bcf3
linrongbin <linrongbin16@gmail.com>
2019-02-02 15:25:21 +0800

    pass lc-83

---
+++ RemoveDuplicatesFromSortedList-83.cpp
@@ -0,0 +1,33 @@
+#include "RemoveDuplicatesFromSortedList-83.h"
+
+int main(void) {
+  Solution s;
+
+  int s1[] = {0, 1, 1, 2, 3};
+  ListNode *l1 = createList(s1, sizeof(s1) / sizeof(int));
+  dumpList(l1);
+  dumpList(s.deleteDuplicates(l1));
+  cout << endl;
+
+  int s2[] = {0, 10, 2, 2, 8, 3, 6, 6, 6};
+  ListNode *l2 = createList(s2, sizeof(s2) / sizeof(int));
+  dumpList(l2);
+  dumpList(s.deleteDuplicates(l2));
+  cout << endl;
+
+  int s3[] = {0, 0, 10, 2, 8, -13, 7, 5, 6};
+  ListNode *l3 = createList(s3, sizeof(s3) / sizeof(int));
+  dumpList(l3);
+  dumpList(s.deleteDuplicates(l3));
+  cout << endl;
+
+  int s4[] = {1, 1};
+  ListNode *l4 = createList(s4, sizeof(s4) / sizeof(int));
+  dumpList(l4);
+  dumpList(s.deleteDuplicates(l4));
+  cout << endl;
+
+  pause();
+  return 0;
+}
+
--- RemoveDuplicatesFromSortedList-83.h
+++ RemoveDuplicatesFromSortedList-83.h
@@ -1,2 +1,32 @@
 #include "LeetCode.h"
 
+class Solution {
+  bool detectDup(ListNode *e) { return e && e->next && e->val == e->next->val; }
+
+  ListNode *removeDup(ListNode *l) {
+    if (!l || !l->next) {
+      return l;
+    }
+    ListNode *p = l->next;
+    while (p && p->val == l->val) {
+      p = p->next;
+    }
+    return p;
+  }
+
+public:
+  ListNode *deleteDuplicates(ListNode *head) {
+    if (!head || !head->next) {
+      return head;
+    }
+    ListNode *p = head;
+    while (p) {
+      while (detectDup(p)) {
+        p->next = p->next->next;
+      }
+      p = p->next;
+    }
+    return head;
+  }
+};
+
