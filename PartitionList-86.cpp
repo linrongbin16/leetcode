920772b30294ae69370cd72fba873afdd22bcbc2
linrongbin <linrongbin16@gmail.com>
2019-02-02 11:49:17 +0800

    refold source code, pass lc-86

---
+++ PartitionList-86.cpp
@@ -0,0 +1,67 @@
+#include "PartitionList-86.h"
+
+ListNode *insertTail(ListNode *l, int x) {
+  if (!l) {
+    return new ListNode(x);
+  }
+  ListNode *e = new ListNode(x);
+  ListNode *p = l;
+  while (p->next) {
+    p = p->next;
+  }
+  p->next = e;
+  return l;
+}
+
+ListNode *createList(int *s, int n) {
+  ListNode *l = NULL;
+  for (int i = 0; i < n; i++) {
+    l = insertTail(l, s[i]);
+  }
+  return l;
+}
+
+void dumpList(ListNode *l) {
+  cout << "dump:";
+  while (l) {
+    cout << l->val << " ";
+    l = l->next;
+  }
+  cout << endl;
+}
+
+int main(void) {
+  Solution s;
+
+  int s1[] = {0, 1, 2, 3}, x1 = 2;
+  ListNode *l1 = createList(s1, sizeof(s1) / sizeof(int));
+  cout << "x1:" << x1 << " ";
+  dumpList(l1);
+  dumpList(s.partition(l1, x1));
+  cout << endl;
+
+  int s2[] = {0, 10, 2, 8, 3, 6, 4, 5}, x2 = 3;
+  ListNode *l2 = createList(s2, sizeof(s2) / sizeof(int));
+  cout << "x2:" << x2 << " ";
+  dumpList(l2);
+  dumpList(s.partition(l2, x2));
+  cout << endl;
+
+  int s3[] = {0, 10, 2, 8, -13, 7, 5, 6}, x3 = 4;
+  ListNode *l3 = createList(s3, sizeof(s3) / sizeof(int));
+  cout << "x3:" << x3 << " ";
+  dumpList(l3);
+  dumpList(s.partition(l3, x3));
+  cout << endl;
+
+  int s4[] = {1, 1}, x4 = 0;
+  ListNode *l4 = createList(s4, sizeof(s4) / sizeof(int));
+  cout << "x4:" << x4 << " ";
+  dumpList(l4);
+  dumpList(s.partition(l4, x4));
+  cout << endl;
+
+  pause();
+  return 0;
+}
+
---
+++ PartitionList-86.h
@@ -0,0 +1,56 @@
+#include "std.h"
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+  ListNode *insertHead(ListNode *l, ListNode *e) {
+    e->next = l;
+    return e;
+  }
+
+  ListNode *reverse(ListNode *l) {
+    if (!l || !l->next) {
+      return l;
+    }
+    ListNode *result = NULL;
+    ListNode *p;
+    while (l) {
+      p = l->next;
+      result = insertHead(result, l);
+      l = p;
+    }
+    return result;
+  }
+
+public:
+  ListNode *partition(ListNode *head, int x) {
+    if (!head || !head->next) {
+      return head;
+    }
+    ListNode *small = NULL, *big = NULL, *p, *tail = NULL;
+    head = reverse(head);
+    while (head) {
+      p = head->next;
+      if (head->val < x) {
+        small = insertHead(small, head);
+        if (!tail) {
+          tail = small;
+        }
+      } else {
+        big = insertHead(big, head);
+      }
+      head = p;
+    }
+    if (tail) {
+      tail->next = big;
+      return small;
+    } else {
+      return big;
+    }
+  }
+};
+
