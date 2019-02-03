c79670261f94255ecc896d889f5b8e4f7587a0e5
linrongbin <linrongbin16@gmail.com>
2019-02-03 23:21:37 +0800

    lc-134 not finished

---
+++ GasStation-134.h
@@ -0,0 +1,10 @@
+#include "LeetCode.h"
+
+class Solution {
+public:
+  int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+    gas.size();
+    cost.size();
+    return gas.size();
+  }
+};
