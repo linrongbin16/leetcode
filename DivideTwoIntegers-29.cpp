a737acdc75e1d01154c04ec31996f9290ba0945f
damu <damu@pinduoduo.com>
2019-02-12 19:22:45 +0800

    pass lc-29

---
+++ DivideTwoIntegers-29.cpp
@@ -0,0 +1,10 @@
+#include "DivideTwoIntegers-29.h"
+
+int main(void) {
+  Solution s;
+  cout << s.divide(1, 1) << endl;
+  cout << s.divide(2, 1) << endl;
+  cout << s.divide(10, 3) << endl;
+  cout << s.divide(7, -3) << endl;
+  return 0;
+}
