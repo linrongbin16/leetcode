a6e432f7f809771efddb971d8e29f771fb186704
linrongbin <linrongbin16@gmail.com>
2019-02-12 09:44:02 +0800

    add lc-118

---
+++ PascalsTriangle-118.cpp
@@ -0,0 +1,7 @@
+#include "PascalsTriangle-118.h"
+
+int main(void) {
+  cout << endl;
+  return 0;
+}
+
---
+++ PascalsTriangle-118.h
@@ -0,0 +1,14 @@
+#include "LeetCode.h"
+
+class Solution {
+public:
+  vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> result;
+    for (int i = 1; i <= numRows; i++) {
+      for (int j = 1; j <= i; j++) {
+      }
+    }
+    return {};
+  }
+};
+
