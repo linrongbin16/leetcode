62968811de6edd6187da1e38ed736d0ba8e5533a
damu <damu@pinduoduo.com>
2019-02-14 20:46:00 +0800

    update lc-117

--- PopulatingNextRightPointersInEachNodeII-117.h
+++ PopulatingNextRightPointersInEachNodeII-117.h
@@ -1,43 +1,19 @@
 #include "LeetCode.h"
 
-// TODO: use O(n) memory
 class Solution {
-  unordered_map<int, vector<TreeLinkNode *>> levelMap_;
-  int level_;
-  int maxLevel_;
-
-  void preorderTraverse(TreeLinkNode *e) {
-    if (!e) {
-      return;
-    }
-    level_++;
-    maxLevel_ = max(maxLevel_, level_);
-    vector<TreeLinkNode *> traverseOrder;
-    if (levelMap_.find(level_) != levelMap_.end()) {
-      traverseOrder = levelMap_[level_];
-    }
-    traverseOrder.push_back(e);
-    levelMap_[level_] = traverseOrder;
-    preorderTraverse(e->left);
-    preorderTraverse(e->right);
-    level_--;
-  }
-
 public:
   void connect(TreeLinkNode *root) {
-    if (!root) {
-      return;
-    }
-    levelMap_.clear();
-    level_ = 0;
-    maxLevel_ = 0;
-    preorderTraverse(root);
-
-    for (auto i = levelMap_.begin(); i != levelMap_.end(); i++) {
-      vector<TreeLinkNode *> traverseOrder = i->second;
-      for (int j = 0; j < traverseOrder.size(); j++) {
-        if (j + 1 < traverseOrder.size()) {
-          traverseOrder[j]->next = traverseOrder[j + 1];
+    TreeLinkNode dummyHead(0);
+    for (; root; root = dummyHead.next, dummyHead.next = NULL) {
+      for (TreeLinkNode *level = root, *last = &dummyHead; level;
+           level = level->next) {
+        if (level->left) {
+          last->next = level->left;
+          last = last->next;
+        }
+        if (level->right) {
+          last->next = level->right;
+          last = last->next;
         }
       }
     }
