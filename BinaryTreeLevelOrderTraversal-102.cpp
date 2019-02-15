a32eb58b898f84c07103c8ec48355d87ef70afe2
damu <damu@pinduoduo.com>
2019-02-15 19:44:46 +0800

    pass lc-102

---
+++ BinaryTreeLevelOrderTraversal-102.cpp
@@ -0,0 +1,16 @@
+#include "BinaryTreeLevelOrderTraversal-102.h"
+
+int main(void) {
+  Solution s;
+
+  int s1[] = {1};
+  dumpVector2D(s.levelOrder(createTree(s1, sizeof(s1) / sizeof(int))));
+
+  int s2[] = {1, 2, 3};
+  dumpVector2D(s.levelOrder(createTree(s2, sizeof(s2) / sizeof(int))));
+
+  int s3[] = {3, 9, 20, INT_MIN, INT_MIN, 15, 7};
+  dumpVector2D(s.levelOrder(createTree(s3, sizeof(s3) / sizeof(int))));
+
+  return 0;
+}
---
+++ BinaryTreeLevelOrderTraversal-102.h
@@ -0,0 +1,37 @@
+#include "LeetCode.h"
+
+class Solution {
+  vector<pair<int, int>> levelMap_;
+  vector<vector<int>> result_;
+  int level_;
+  int depth_;
+
+  void preorder(TreeNode *e) {
+    if (!e) {
+      return;
+    }
+    level_++;
+    depth_ = max(depth_, level_);
+    levelMap_.push_back(make_pair(e->val, level_ - 1));
+    preorder(e->left);
+    preorder(e->right);
+    level_--;
+  }
+
+public:
+  vector<vector<int>> levelOrder(TreeNode *root) {
+    levelMap_.clear();
+    result_.clear();
+    level_ = 0;
+    depth_ = 0;
+    if (!root) {
+      return result_;
+    }
+    preorder(root);
+    result_.resize(depth_);
+    for (auto i = levelMap_.begin(); i != levelMap_.end(); i++) {
+      result_[i->second].push_back(i->first);
+    }
+    return result_;
+  }
+};
