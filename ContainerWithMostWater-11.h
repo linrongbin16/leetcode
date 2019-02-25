4f5c41d69d40c3a9100d320713dcf4bb51178cd1
linrongbin <linrongbin16@gmail.com>
2019-02-25 17:56:27 +0800

    optimize lc-11

--- ContainerWithMostWater-11.h
+++ ContainerWithMostWater-11.h
@@ -7,12 +7,16 @@
 class Solution {
 public:
   int maxArea(vector<int> &height) {
-    int result = 0;
-    for (int i = 0; i < height.size(); i++) {
-      for (int j = i + 1; j < height.size(); j++) {
-        result = max(result, min(height[i], height[j]) * (j - i));
-      }
+    int water = 0;
+    int i = 0, j = height.size() - 1;
+    while (i < j) {
+      int h = min(height[i], height[j]);
+      water = max(water, (j - i) * h);
+      while (height[i] <= h && i < j)
+        i++;
+      while (height[j] <= h && i < j)
+        j--;
     }
-    return result;
+    return water;
   }
 };
