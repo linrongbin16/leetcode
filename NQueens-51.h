14c0dfc2e087e9132463cdc0048433909c0dbfd2
linrongbin <linrongbin16@gmail.com>
2019-02-26 22:45:45 +0800

    save lc-51

--- NQueens-51.h
+++ NQueens-51.h
@@ -7,32 +7,76 @@
 class Solution {
   int n_;
   vector<vector<string>> result_;
-  vector<int> visit_;
   char **tab_;
 
-  vector<string> parse(vector<int> &s) {
+  void createTable() {
+    tab_ = new char *[n_];
+    for (int i = 0; i < n_; i++) {
+      tab_[i] = new char[n_];
+    }
+  }
+
+  void freeTable() {
+    for (int i = 0; i < n_; i++) {
+      delete[] tab_[i];
+    }
+    delete[] tab_;
+  }
+
+  vector<string> parse() {
     vector<string> r;
     for (int i = 0; i < n_; i++) {
-      string t(n_, '.');
-      t[s[i]] = 'Q';
+      string t = "";
+      for (int j = 0; j < n_; j++) {
+        t = t + tab_[i][j];
+      }
       r.push_back(t);
     }
     return r;
   }
 
-  void recursive(vector<int> &s, int p) {
+  bool validPos(int i, int j) {
+    for (int k = 0; k < n_; k++) {
+      if (tab_[i][k] == 'Q') {
+        return false;
+      }
+      if (tab_[k][j] == 'Q') {
+        return false;
+      }
+    }
+    for (int k = 0; k < n_; k++) {
+      if ((i - k) >= 0 && (i - k) < n_ && (j - k) >= 0 && (j - k) < n_ &&
+          tab_[i - k][j - k] == 'Q') {
+        return false;
+      }
+      if ((i - k) >= 0 && (i - k) < n_ && (j + k) >= 0 && (j + k) < n_ &&
+          tab_[i - k][j + k] == 'Q') {
+        return false;
+      }
+      if ((i + k) >= 0 && (i + k) < n_ && (j + k) >= 0 && (j + k) < n_ &&
+          tab_[i + k][j + k] == 'Q') {
+        return false;
+      }
+      if ((i + k) >= 0 && (i + k) < n_ && (j - k) >= 0 && (j - k) < n_ &&
+          tab_[i + k][j - k] == 'Q') {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  void recursive(int p) {
     if (p == n_) {
-      result_.push_back(parse(s));
+      result_.push_back(parse());
       return;
     }
     for (int i = 0; i < n_; i++) {
-      if (visit_[i]) {
+      if (validPos(p, i)) {
         continue;
       }
-      visit_[i] = 1;
-      s[p] = i;
-      recursive(s, p + 1);
-      visit_[i] = 0;
+      tab_[p][i] = 'Q';
+      recursive(p + 1);
+      tab_[p][i] = '.';
     }
   }
 
@@ -40,12 +84,9 @@ public:
   vector<vector<string>> solveNQueens(int n) {
     result_.clear();
     n_ = n;
-    visit_.resize(n_);
-    for (int i = 0; i < n_; i++) {
-      visit_[i] = 0;
-    }
-    vector<int> tmp(n_);
-    recursive(tmp, 0);
+    createTable();
+    recursive(0);
+    freeTable();
     return result_;
   }
 };
