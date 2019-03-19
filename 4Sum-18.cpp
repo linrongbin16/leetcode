ff781b89589ab8d8ea5cddd9b772051d086ecef2
linrongbin <linrongbin16@gmail.com>
2019-03-19 20:49:36 +0800

    add lc-18

---
+++ 4Sum-18.cpp
@@ -0,0 +1,10 @@
+#include "4Sum-18.h"
+
+int main(void) {
+  Solution s;
+
+  vector<int> v1 = {1, 0, -1, 0, -2, 2};
+  dumpVector2D<int>(s.fourSum(v1, 0));
+
+  return 0;
+}
--- 4Sum-18.h
+++ 4Sum-18.h
@@ -6,11 +6,35 @@
 
 class Solution {
   int *tb_;
+  vector<vector<int>> result_;
+
+  void recursion(vector<int> &nums, int target, int *tb, int p) {
+    if (p == nums.size()) {
+      int sum = 0;
+      for (int i = 0; i < nums.size(); i++) {
+        sum += (tb[i]) ? nums[i] : 0;
+      }
+      if (sum == target) {
+        result_.push_back(nums);
+      }
+      return;
+    }
+
+    for (int x = 0; x < 2; x++) {
+      tb[p] = x;
+      recursion(nums, target, tb, p + 1);
+    }
+  }
 
 public:
   vector<vector<int>> fourSum(vector<int> &nums, int target) {
-    vector<vector<int>> result;
+    result_.clear();
     tb_ = new int[nums.size()];
-    return result;
+    for (int i = 0; i < nums.size(); i++) {
+      tb_[i] = 0;
+    }
+    recursion(nums, target, tb_, 0);
+    delete[] tb_;
+    return result_;
   }
 };
