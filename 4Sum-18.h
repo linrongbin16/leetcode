31f4fe8ee32a78fb73bad17646ff5678dde947bc
linrongbin <linrongbin16@gmail.com>
2019-03-19 09:57:02 +0800

    add lc-18

---
+++ 4Sum-18.h
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/4sum/
+ */
+
+#include "LeetCode.h"
+
+class Solution {
+  int *tb_;
+
+public:
+  vector<vector<int>> fourSum(vector<int> &nums, int target) {
+    vector<vector<int>> result;
+    tb_ = new int[nums.size()];
+    return result;
+  }
+};
