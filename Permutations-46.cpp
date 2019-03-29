e74b739fd63254ec39b319a0fb7af4d2dd41f59d
linrongbin <linrongbin16@gmail.com>
2019-03-29 08:03:45 +0800

    pass lc-46

---
+++ Permutations-46.cpp
@@ -0,0 +1,9 @@
+#include "Permutations-46.h"
+
+int main(void) {
+  Solution s;
+
+  vector<int> v1 = {1, 2, 3};
+  dumpVector2D(s.permute(v1));
+  return 0;
+}
--- Permutations-46.h
+++ Permutations-46.h
@@ -5,9 +5,23 @@
 #include "LeetCode.h"
 
 class Solution {
+
+  void recursive(vector<vector<int>> &result, vector<int> &nums, int p) {
+    if (p == nums.size()) {
+      result.push_back(nums);
+      return;
+    }
+    for (int i = 0; i <= p; i++) {
+      swap(nums[i], nums[p]);
+      recursive(result, nums, p + 1);
+      swap(nums[i], nums[p]);
+    }
+  }
+
 public:
   vector<vector<int>> permute(vector<int> &nums) {
     vector<vector<int>> result;
+    recursive(result, nums, 0);
     return result;
   }
 };
