82e2399024612b061490489a648d9c860438e413
linrongbin <linrongbin16@gmail.com>
2019-03-31 09:31:36 +0800

    pass lc-48

--- RotateImage-48.h
+++ RotateImage-48.h
@@ -6,7 +6,6 @@
 
 class Solution {
 
-  // scale *2
   pair<int, int> lastPosImpl(int x, int y, int n) {
     int c = n / 2;
     int diffx = abs(x - c), diffy = abs(y - c);
@@ -16,24 +15,25 @@ class Solution {
     }
     // 1st part
     if (x < c && y <= c) {
-      return make_pair(c + diffy, c - diffx);
+      return make_pair(c - diffy, c + diffx);
     }
     // 2nd part
     if (x <= c && y > c) {
-      return make_pair(c - diffy, c - diffx);
+      return make_pair(c + diffy, c + diffx);
     }
     // 3rd part
     if (x > c && y >= c) {
-      return make_pair(c - diffy, c + diffx);
+      return make_pair(c + diffy, c - diffx);
     }
     // 4th part
     if (x >= c && y < c) {
-      return make_pair(c + diffy, c + diffx);
+      return make_pair(c - diffy, c - diffx);
     }
     assert(false);
     return make_pair(0, 0);
   }
 
+  // scale *2
   pair<int, int> lastPos(int x, int y, int n) {
     pair<int, int> r = lastPosImpl(2 * x, 2 * y, 2 * n);
     return make_pair(r.first / 2, r.second / 2);
@@ -41,13 +41,30 @@ class Solution {
 
 public:
   void rotate(vector<vector<int>> &matrix) {
-    for (int i = 0; i <= matrix.size() / 2; i++) {
-      for (int j = 0; j <= matrix.size() / 2; j++) {
+    for (int i = 0; i < matrix.size() / 2; i++) {
+      for (int j = 0; j < matrix.size() / 2; j++) {
         pair<int, int> r = make_pair(i, j);
         int tmp = matrix[i][j];
         // repeat 4 times
         for (int k = 0; k < 4; k++) {
+          cout << "[" << r.first << "," << r.second << "]" << tmp;
           r = lastPos(r.first, r.second, matrix.size() - 1);
+          cout << " ->[" << r.first << "," << r.second << "]"
+               << matrix[r.first][r.second] << endl;
+          swap(matrix[r.first][r.second], tmp);
+        }
+      }
+    }
+    if (matrix.size() % 2) {
+      for (int i = 0, j = matrix.size() / 2; i < matrix.size() / 2; i++) {
+        pair<int, int> r = make_pair(i, j);
+        int tmp = matrix[i][j];
+        // repeat 4 times
+        for (int k = 0; k < 4; k++) {
+          cout << "[" << r.first << "," << r.second << "]" << tmp;
+          r = lastPos(r.first, r.second, matrix.size() - 1);
+          cout << " ->[" << r.first << "," << r.second << "]"
+               << matrix[r.first][r.second] << endl;
           swap(matrix[r.first][r.second], tmp);
         }
       }
