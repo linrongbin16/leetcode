fddf990c7a0d02c45214d8b082bc843c7dc154bc
linrongbin <linrongbin16@gmail.com>
2019-04-09 17:09:14 +0800

    pass lc-56

--- MergeIntervals-56.h
+++ MergeIntervals-56.h
@@ -5,32 +5,15 @@
 #include "LeetCode.h"
 
 class Solution {
-  bool conflict(Interval &a, Interval &b) {
-    return a.end >= b.start || a.start <= b.end;
-  }
-
-  pair<bool, vector<Interval>> mergeImpl(vector<Interval> &intervals, int p) {
-    for (int i = 0; i < intervals.size(); i++) {
-      if (conflict(intervals[i], intervals[p])) {
-        vector<Interval> result;
-        for (int j = 0; j < intervals.size(); j++) {
-          if (j != i && j != p) {
-            result.push_back(intervals[j]);
-          }
-        }
-        int start = min(intervals[i].start, intervals[p].start);
-        int end = min(intervals[i].end, intervals[p].end);
-        result.push_back(Interval(start, end));
-        return make_pair(true, result);
-      }
-    }
-    return make_pair(false, vector<Interval>());
+  bool intersect(const Interval &a, const Interval &b) {
+    return (a.start <= b.start && a.end >= b.start) ||
+           (b.start <= a.start && b.end >= a.start);
   }
 
   pair<int, int> search(vector<Interval> &intervals) {
     for (int i = 0; i < intervals.size(); i++) {
       for (int j = i + 1; j < intervals.size(); j++) {
-        if (conflict(intervals[i], intervals[j])) {
+        if (intersect(intervals[i], intervals[j])) {
           return make_pair(i, j);
         }
       }
